91d1fdee69c3c74d3fdbc2ef11254238bcadc01d
naoto613 <fujii140@gmail.com>
2023-02-28 23:02:49 +0900

    chore(front): pagesを修正

--- apps/front/pages/_app.tsx
+++ apps/front/pages/_app.tsx
@@ -1,18 +1,34 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
+import './styles.css'
+import { createClient, Provider } from 'urql'
+
+/** APIのエンドポイント */
+const url = process.env.NEXT_PUBLIC_API_URL
+
+const client = () => {
+  return createClient({
+    url,
+    fetchOptions: {
+      credentials: 'include',
+    },
+  })
+}
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>Welcome to front!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
+    <Provider value={client()}>
+      <ChakraProvider>
+        <Head>
+          <title />
+        </Head>
+        <main className={'app'}>
+          <Component {...pageProps} />
+        </main>
+      </ChakraProvider>
+    </Provider>
+  )
 }
 
-export default CustomApp;
+export default CustomApp
