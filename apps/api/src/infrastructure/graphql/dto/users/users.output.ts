db06d77aeb6318a8509dbe202ca1336ad57bd15c
naoto613 <fujii140@gmail.com>
2023-03-01 21:15:35 +0900

    chore(api): ユーザーのDTO修正

--- apps/api/src/infrastructure/graphql/dto/users/users.output.ts
+++ apps/api/src/infrastructure/graphql/dto/users/users.output.ts
@@ -1,11 +1,20 @@
 import { UserOutput } from '@/infrastructure/graphql/dto/users/user.output'
-import { Field, Int, ObjectType } from '@nestjs/graphql'
+import { Field, Int, ObjectType, PickType } from '@nestjs/graphql'
+
+@ObjectType()
+class DataForUserOutput extends PickType(UserOutput, [
+  'email',
+  'name',
+] as const) {}
 
 @ObjectType()
 export class UsersOutput {
   @Field(() => Int, { nullable: false, description: '取得件数' })
   count: number
 
-  @Field(() => [UserOutput], { nullable: false, description: 'ユーザー' })
-  data: Array<UserOutput>
+  @Field(() => [DataForUserOutput], {
+    nullable: false,
+    description: 'ユーザー',
+  })
+  data: Array<DataForUserOutput>
 }
