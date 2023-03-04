4013d80f9f3739a512b2ce1ac0384d95cbec49a9
naoto613 <fujii140@gmail.com>
2023-03-04 14:02:50 +0900

    test(api): test coverageの設定を追加

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -13,4 +13,13 @@ export default {
   },
   moduleFileExtensions: ['ts', 'js', 'html', 'json'],
   coverageDirectory: '../../coverage/apps/api',
+  collectCoverageFrom: [
+    '**/*.{tsx,ts}',
+    '!jest.config.ts',
+    '!src/main.ts',
+    '!**/*.dto.ts',
+    '!./src/infrastructure/prisma/**/*',
+    '!./src/infrastructure/plugins/**/*',
+    '!./src/infrastructure/graphql/**/*',
+  ],
 }
